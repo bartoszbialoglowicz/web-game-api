9e4b0ae83bb7e5f530a2b11dfed51f3b70483e0e
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2021-11-18 22:21:01 +0100

    Created user creation view.

--- user/views.py
+++ user/views.py
@@ -1,2 +1,7 @@
+from rest_framework import generics, authentication, permissions
+
+from user import serializers
 
 
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
