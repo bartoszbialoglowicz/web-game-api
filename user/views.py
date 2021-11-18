c210742048fc1523dfe101250390af8280a936dc
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2021-11-18 22:52:28 +0100

    Created Token serializer.

--- user/views.py
+++ user/views.py
@@ -1,7 +1,14 @@
 from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user import serializers
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = serializers.UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = serializers.AuthTokenSerializer
+    renderer_classes = api_settings
