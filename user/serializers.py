c6f4b0aba04f6fabbf64712d85af3e9760c1a1f8
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2021-11-18 23:15:42 +0100

    Created user endpoint.

--- user/serializers.py
+++ user/serializers.py
@@ -28,13 +28,15 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
     email = serializers.CharField()
-    pasword = serializers.CharField(
+    password = serializers.CharField(
         style={'input_type': 'password'},
         trim_whitespace=False
     )
 
     def validate(self, attrs):
+        """Validate and authenticate the user"""
         email = attrs.get('email')
         password = attrs.get('password')
 
@@ -43,9 +45,8 @@ class AuthTokenSerializer(serializers.Serializer):
             username=email,
             password=password
         )
-        
         if not user:
-            msg = _("Unable to authenticate with provided credentials")
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -11,4 +12,15 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     serializer_class = serializers.AuthTokenSerializer
-    renderer_classes = api_settings
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = serializers.UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
